1b6165f6eaa24a7a403b700b0ac4a8f3315501b8
phuongnhi2252001 <84986109+phuongnhi2252001@users.noreply.github.com>
2022-03-13 15:25:04 +0700

    BaiTapCoBan2/Bai1

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a140, b140;
+            Console.Write("Nhap dien tich mat cau = ");
+            a140 = double.Parse(Console.ReadLine());
+            b140 = (4 * 3.14 * Math.Pow(Math.Sqrt(a140 / (4 * 3.14)), 3)) / 3;
+            Console.WriteLine($"The tich V = {b140}");
+            Console.ReadKey();
+        }
+    }
+}

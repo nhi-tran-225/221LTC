0cb108b2fec6aa7f92ef6f6584100cc8a8aef053
phuongnhi2252001 <84986109+phuongnhi2252001@users.noreply.github.com>
2022-03-13 15:25:46 +0700

    BaiTapCoBan2/Bai2

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a140, b140, x140, y140, kq140;
+            Console.Write("Nhap toa do diem A  ");
+            a140 = double.Parse(Console.ReadLine());
+            b140 = double.Parse(Console.ReadLine());
+            Console.Write("Nhap toa do diem B  ");
+            x140 = double.Parse(Console.ReadLine());
+            y140 = double.Parse(Console.ReadLine());
+            kq140 = Math.Sqrt((x140 - a140) * (x140 - a140) + (y140 - b140) * (y140 - b140));
+            Console.WriteLine($"Khoang cach AB = {kq140}");
+            Console.ReadKey();
+        }
+    }
+}

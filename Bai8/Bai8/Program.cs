40dbf6e87739bc231d2cd8cc7bb17d8c4a4961f7
phuongnhi2252001 <84986109+phuongnhi2252001@users.noreply.github.com>
2022-03-13 15:27:06 +0700

    BaiTapCoBan2/Bai8

---
+++ Bai8/Bai8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bai8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a140, b140, c140;
+            Console.WriteLine("Nhap vao a : ");
+            a140 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao b : ");
+            b140 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap vao c : ");
+            c140 = float.Parse(Console.ReadLine());
+            float delta140 = b140 * b140 - 4 * a140 * c140;
+            if (delta140 > 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem : ");
+                Console.WriteLine("X1 = {0}", ((-b140 - Math.Sqrt(delta140)) / 2 * a140));
+                Console.WriteLine("X2 = {0}", ((-b140 + Math.Sqrt(delta140)) / 2 * a140));
+
+            }
+            else if (delta140 == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep nghiem");
+                Console.WriteLine("X1 = X2 {0}", -b140 / 2 * a140);
+            }
+            else if (delta140 < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+
+        }
+    }
+}

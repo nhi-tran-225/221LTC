ae3ef9296bb704f4b48bf89d026d765efed908e5
phuongnhi2252001 <84986109+phuongnhi2252001@users.noreply.github.com>
2022-03-13 15:28:51 +0700

    BaiTapCoBan2/Bai18

---
+++ Bai18/Bai18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long h140, t140, n140;
+            Console.Write("Nhap so gio: ");
+            h140 = long.Parse(Console.ReadLine());
+
+            t140 = h140 / (24 * 7);
+            n140 = (h140 % (24 * 7)) / 24;
+            h140 = (h140 % (24 * 7)) % 24;
+
+            Console.WriteLine($"{t140} tuan, {n140} ngay, {h140} gio");
+            Console.ReadKey();
+        }
+    }
+}

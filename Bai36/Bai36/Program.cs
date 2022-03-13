83a6375ec8042be3a2f9dd14d6e2a884ad75c3e8
phuongnhi2252001 <84986109+phuongnhi2252001@users.noreply.github.com>
2022-03-13 15:33:00 +0700

    BaiTapCoBan2/Bai36

---
+++ Bai36/Bai36/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bai36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n140, k140, i140, count140;
+            Console.WriteLine("Nhap n: ");
+            n140 = long.Parse(Console.ReadLine());
+            k140 = 2;
+            count140 = 0;
+            while (count140 < n140)
+            { /* vòng lặp kiểm tra k có phải là số nguyên tố */
+                for (i140 = 2; i140 * i140 <= k140; ++i140)
+                    if (k140 % i140 == 0) break;
+                if (i140 * i140 > k140)
+                {
+                    Console.WriteLine($"{k140}");
+                    count140++;
+                }
+                k140++;
+            }
+        }
+    }
+}
